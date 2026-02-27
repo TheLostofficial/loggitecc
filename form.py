7dc30d333116d90c03eaf66e3b69b148a06b845d
TheLostofficial <101384319+TheLostofficial@users.noreply.github.com>
2026-02-27 18:58:28 +0000

    Okak

---
+++ form.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Brand, Category, Format, Product
+
+
+class BrandForm(forms.ModelForm):
+    class Meta:
+        model = Brand
+        fields = ['name', 'country']
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class FormatForm(forms.ModelForm):
+    class Meta:
+        model = Format
+        fields = ['name']
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'price',
+            'weight',
+            'servings',
+            'brand',
+            'category',
+            'format',
+            'image',
+        ]
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 5}),
+            'image': forms.FileInput(),
+        }
+        
